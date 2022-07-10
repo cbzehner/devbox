ccd8c095c4a98d4d99bdc96be8081e0fbb3d9ada
Chris Zehner <cbzehner@gmail.com>
2022-07-09 19:44:19 -0500

    setup email DNS records

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -36,3 +36,61 @@ resource "cloudflare_record" "wildcard" {
   type    = "CNAME"
   proxied = false
 }
+
+resource "cloudflare_record" "mail-exchanger-one" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "@"
+  value    = "in1-smtp.messagingengine.com"
+  priority = 10
+  type     = "MX"
+  proxied  = false
+}
+
+resource "cloudflare_record" "mail-exchanger-two" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "@"
+  value    = "in2-smtp.messagingengine.com"
+  priority = 20
+  type     = "MX"
+  proxied  = false
+}
+
+resource "cloudflare_record" "verify-ownership-for-fastmail-one" {
+  zone_id = var.cloudflare_zone_id
+  name    = "fm1._domainkey"
+  value   = "fm1.commitracer.com.dkim.fmhosted.com"
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "verify-ownership-for-fastmail-two" {
+  zone_id = var.cloudflare_zone_id
+  name    = "fm2._domainkey"
+  value   = "fm2.commitracer.com.dkim.fmhosted.com"
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "verify-ownership-for-fastmail-three" {
+  zone_id = var.cloudflare_zone_id
+  name    = "fm3._domainkey"
+  value   = "fm3.commitracer.com.dkim.fmhosted.com"
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "sender-policy-framework" {
+  zone_id = var.cloudflare_zone_id
+  name    = "@"
+  value   = "v=spf1 include:spf.messagingengine.com ?all"
+  type    = "TXT"
+  proxied = false
+}
+
+resource "cloudflare_record" "dmarc" {
+  zone_id = var.cloudflare_zone_id
+  name    = "_dmarc"
+  value   = "v=DMARC1; p=quarantine; rua=mailto:admin@commitracer.com"
+  type    = "TXT"
+  proxied = false
+}
